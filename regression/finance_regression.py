fbb7a3dd5054cf02df70098de1ca4044d7568efa
Sven Koerbitz <skoerbitz@gmail.com>
2017-01-22 17:42:26 +0100

    Lecture: Regressions

--- regression/finance_regression.py
+++ regression/finance_regression.py
@@ -29,7 +29,7 @@ target, features = targetFeatureSplit( data )
 from sklearn.cross_validation import train_test_split
 feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
 train_color = "b"
-test_color = "b"
+test_color = "r"
 
 
 
@@ -38,10 +38,16 @@ test_color = "b"
 ### plots it correctly. Don't forget to change the test_color above from "b" to
 ### "r" to differentiate training points from test points.
 
+from sklearn.linear_model import LinearRegression
 
+reg = LinearRegression()
+reg.fit(feature_train, target_train)
 
+print "Slope: ", reg.coef_
+print "Intercept: ", reg.intercept_
 
-
+print "Score train set: ",reg.score(feature_train, target_train)
+print "Score test set: ",reg.score(feature_test, target_test)
 
 
 
@@ -56,10 +62,6 @@ for feature, target in zip(feature_train, target_train):
 plt.scatter(feature_test[0], target_test[0], color=test_color, label="test")
 plt.scatter(feature_test[0], target_test[0], color=train_color, label="train")
 
-
-
-
-### draw the regression line, once it's coded
 try:
     plt.plot( feature_test, reg.predict(feature_test) )
 except NameError:
