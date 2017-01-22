f73854c992b84c3c27259464490782c2c936e553
Sven Koerbitz <skoerbitz@gmail.com>
2017-01-22 18:45:41 +0100

    Lecture: KMeans & Feature Scaling

--- k_means/clustering_project_scaled.py
+++ k_means/clustering_project_scaled.py
@@ -38,36 +38,41 @@ data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r")
 data_dict.pop("TOTAL", 0)
 
 
-### the input features we want to use 
-### can be any key in the person-level dictionary (salary, director_fees, etc.) 
-feature_1 = "salary"
-feature_2 = "exercised_stock_options"
-feature_3 = "total_payments"
-poi  = "poi"
-features_list = [poi, feature_1, feature_2, feature_3]
-data = featureFormat(data_dict, features_list )
-poi, finance_features = targetFeatureSplit( data )
+### Quiz: Feature scaling using MinMaxScaler
 
+## Salary Range
+salary = []
 
-### in the "clustering with 3 features" part of the mini-project,
-### you'll want to change this line to 
-### for f1, f2, _ in finance_features:
-### (as it's currently written, the line below assumes 2 features)
-for f1, f2, f3 in finance_features:
-    plt.scatter( f1, f2, f3)
-plt.show()
+for users in data_dict:
+    value = data_dict[users]["salary"]
+    if value == 'NaN':
+        continue
+    salary.append(float(value))
 
-### cluster here; create predictions of the cluster labels
-### for the data and store them to a list called pred
-from sklearn.cluster import KMeans
-kmeans = KMeans(n_clusters=2)
-kmeans.fit(finance_features)
-pred = kmeans.predict(finance_features)
+min_salary = min(salary)
+max_salary = max(salary)
 
-### rename the "name" parameter when you change the number of features
-### so that the figure gets saved to a different file
-try:
-    Draw(pred, finance_features, poi, mark_poi=False, name="clusters.pdf", f1_name=feature_1, f2_name=feature_2)
-except NameError:
-    print "no predictions object named pred found, no clusters to plot"
+## Excercised Stock Options Range
+eso = []
+
+for users in data_dict:
+    value = data_dict[users]["exercised_stock_options"]
+    if value == 'NaN':
+        continue
+    eso.append(float(value))
+
+min_eso = min(eso)
+max_eso = max(eso)
+
+## Scaling of the Features (to get scaled values for Salary of 200.000 & ESO of 1.000.000)
+from sklearn.preprocessing import MinMaxScaler
+scaler = MinMaxScaler()
+
+salaries = numpy.array([[float(min_salary)], [200000.0], [float(max_salary)]])
+salaries_scaled = scaler.fit_transform(salaries)
+print salaries_scaled
+
+ex_stock_options = numpy.array([[float(min_eso)], [1000000.0], [float(max_eso)]])
+ex_stock_options_scaled = scaler.fit_transform(ex_stock_options)
+print ex_stock_options_scaled
 
