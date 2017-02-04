97124bc9a3790e9b8d3dd375ac7bf5f909692051
Sven Koerbitz <skoerbitz@gmail.com>
2017-02-04 18:39:04 +0100

    Corrected typo 'labels'

--- validation/validate_poi.py
+++ validation/validate_poi.py
@@ -28,7 +28,7 @@ features_list = ["poi", "salary"]
 data = featureFormat(data_dict, features_list)
 labels, features = targetFeatureSplit(data)
 
-features_train, features_test, labes_train, labels_test = train_test_split(features, labels, test_size=0.3, random_state=42)
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=0.3, random_state=42)
 
 clf = DecisionTreeClassifier()
 clf.fit(features_train,labels_train)
